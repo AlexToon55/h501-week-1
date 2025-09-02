dcf3c829a837b36a56a255d151cc64ed730ece7e
Alex Toon <altoon@IU.EDU>
2025-09-02 17:47:47 -0400

    Update file
    Updated the correct file this time. Almost only used the scratch area.
    
    tidied up with headers for each function.

--- apputil.py
+++ apputil.py
@@ -1,3 +1,19 @@
 
 
-# add code below ...
+# Week 1 Exercise 1
+def palindrome(word) -> bool:
+    s = "".join(filter(str.isalnum, word)).lower()
+    return s == s[::-1]
+
+
+# Week 1 Exercise 2
+def parentheses(string: str) -> bool:
+    count = 0
+    for char in string:
+        if char == '(':
+            count += 1
+        elif char == ')':
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
